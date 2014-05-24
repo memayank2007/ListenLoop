af1a08d33ff41e8bf3896958e20e83d05609a5d1
Mayank Mishra <memayank2007@gmail.com>
2014-05-24 10:48:16 +0530

    repo setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ListenLoop::Application.config.secret_key_base = 'f5d0a6aa03325952288adb627f4ee6f665311b25e02e96c49b226c9826005642eaa08f1b586f942b8b3f09bbb1916a65ca11ab8bafdf799a533c9021196bd21f'
